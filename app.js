7cec6011742c43ab99bf48d73f3bc4283cf2d406
Jason Grigg <boatdreamin@gmail.com>
2017-06-22 15:04:56 -0400

    completed list work

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const list = require('./data.js');
+const clist = require('./completedList.js');
+const mustacheExpress = require('mustache-express');
+var bodyParser = require('body-parser');
+const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'mustache')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+function pushToArray(string, num){
+  let tempTask = {'item': string,
+"id":''+num};
+  return tempTask;
+}
+
+app.get('/todo/', function (req, res) {
+
+  res.render('todo', {todoList: list.todoList,
+  completedList: list.completedList})
+});
+
+app.post('/', function(req, res){
+  let num = list.todoList.length;
+  let nextTask = pushToArray(req.body.todo, num);
+  list.todoList.push(nextTask);
+
+
+  res.redirect('/todo');
+
+  });
+
+app.listen(3000, function(){
+  console.log('Started express application!')
+});
---
+++ data.js
@@ -0,0 +1,11 @@
+module.exports = {
+  todoList: [
+    {"item":"Learn Node Basics ", "id":"0"},
+    {"item":"Learn Express Basics ", "id":"1"},
+    {"item":"Learn Mustache ", "id":"2"},
+    {"item":"Learn HTML Forms with Express ", "id":"3"},
+    {"item":"Learn about authentication ", "id":"4"},
+    {"item":"Learn how to create databases ", "id":"5"},
+    {"item":"Learn SQL ", "id":"6"}
+  ]
+}
